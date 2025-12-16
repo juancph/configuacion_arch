0c1ffd9d210f007b677c33c751622cd0bd4fb1cb
juancph <juancarlospinilla35@gmail.com>
2025-12-16 15:30:05 -0500

    lemonbar: Reducir longitud de ssid

--- lemonbar/bar.sh
+++ lemonbar/bar.sh
@@ -21,8 +21,15 @@ while true; do
     #Hora y fecha -------------------------------------------------
     hora=$(date +"%H:%M:%S | %d-%m-%Y")
 
-    disk=$(df -h | awk '/sda6/ {print $4}') 
+    #Volumen ------------------------------------------------------
+    volumen=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume/ {print $5}')
 
+
+    #Memoria ----------------------------------------------------
+    memoria=$(free -m | awk '/Mem:/ {gsub(",", "."); print int($3*100/$2)}')
+
+
+    #Bateria ------------------------------------------------------
     bateria="$(cat /sys/class/power_supply/BAT0/capacity)" 
     bateria_estado=$(cat /sys/class/power_supply/BAT0/status)
 
@@ -35,24 +42,20 @@ while true; do
     #Wifi ------------------------------------------------------
     ssid=$(nmcli -t -f ACTIVE,SSID dev wifi | grep sí | cut -d: -f2)
     estadoRed=$(nmcli -t -f CONNECTIVITY general)
+
+    if [ ${#ssid} -gt 5 ]; then
+        ssid="${ssid:0:5}..."
+    fi
     
     if [ $estadoRed = full ]; then
-        ssid="%{F#00ff00}$(nmcli -t -f ACTIVE,SSID dev wifi | grep sí | cut -d: -f2)%{F-}"
+        ssid="%{F#00ff00}$ssid%{F-}"
     else
         ssid="%{F#ff0000}$estadoRed%{F-}"
     fi
     
 
-    #Memoria ----------------------------------------------------
-    memoria=$(
-        free -m | awk '/Mem:/ {
-            gsub(",", ".");
-            print int(($3*100) / $2)
-        }'
-        )
-
-
     #Salida ----------------------------------------------------
-    echo "%{l}$(workSpaces) %{c}$hora %{r}$ssid | $disk/ | Mem: $memoria% | Bat: $bateria%"
+    echo "%{l}$(workSpaces) %{c}$hora %{r}$ssid | Vol: $volumen | Mem: $memoria% | ${bateria_estado:0:4}: $bateria%"
+
     sleep 1
 done | lemonbar | sh
